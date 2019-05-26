0f5e3a7f607480619e686f5bbfdbcdf526d276ff
Jagdish0886 <jagdishbelapure@gmail.com>
2019-05-26 19:53:38 +0530

    managing state

--- providers.tf
+++ providers.tf
@@ -2,3 +2,14 @@ provider "aws" {
   version = "~> 2.0"
   region = "ap-south-1"
 }
+
+terraform {
+  required_version = ">= 0.12.0"
+
+backend "s3" {
+  bucket ="awslearning-test-ap-south-1"
+  key ="test/backbone"
+  region ="ap-south-1"
+  encrypt ="true"
+}
+}
